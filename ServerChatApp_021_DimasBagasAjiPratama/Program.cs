e87ee1b044e1ad46bcb3b833d15099719fe30f50
DimasBagasAjipratama <56101230+DimasBagasAjipratama@users.noreply.github.com>
2021-01-14 22:27:55 +0700

    Membuat server pesan

--- ServerChatApp_021_DimasBagasAjiPratama/Program.cs
+++ ServerChatApp_021_DimasBagasAjiPratama/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_021;
 
 namespace ServerChatApp_021_DimasBagasAjiPratama
 {
@@ -10,6 +12,23 @@ namespace ServerChatApp_021_DimasBagasAjiPratama
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
